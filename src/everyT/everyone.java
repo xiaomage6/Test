b2067573ceccf7772cc10e8b62f4793347c3a900
ma <m15910897524@163.com>
2018-05-14 10:20:33 +0800

    我的第一个项目

---
+++ src/everyT/everyone.java
@@ -0,0 +1,12 @@
+package everyT;
+
+import java.util.Random;
+
+public class everyone {
+public static void main(String[] args) {
+	int i=1;
+Random random=new Random();
+int count=random.nextInt(5000)*100;
+System.out.println(count);
+}
+}
---
+++ src/everyT/test01.java
@@ -0,0 +1,7 @@
+package everyT;
+
+public class test01 {
+public static void main(String[] args) {
+	
+}
+}
